41f8548a0cc9776daf91439ce6eb340acd70aab1
sistemas <sistemas@VRE_LAB20828.inacap.cl>
2024-12-02 09:34:35 -0300

    creacion defs :3

--- biblioteca_citas/citas/views.py
+++ biblioteca_citas/citas/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, request, get_object_or_404, redirect
+from citas import models as M
 
-def hola():
+def citas_ver(request):
     pass
 
-# Create your views here.
+def citas_agregar(request):
+    pass
+
+def citas_actualizar(request):
+    pass
+
+def citas_eliminar(request):
+    pass
